dc9e4f033fad1d600325cd22f138d4c4be9bf63a
Derek Anderson <derek.anderson@lge.com>
2015-01-27 16:51:06 -0800

    added some callbacks for custom element

--- src/webcomponent.js
+++ src/webcomponent.js
@@ -1,67 +1,84 @@
 //todo: renderInto inherited from component, breaks webcomponent prototype
 
-define(['./component', 'ichimon/main', 'yobidashi/main'], function(component, mixins, messages) {
+define(['./component', 'ichimon/main', 'yobidashi/main'], function (component, mixins, messages) {
 
-    var webcomponent = mixins.mix(HTMLElement.prototype, component);
+	var webcomponent = mixins.mix(HTMLElement.prototype, component);
 
+	//define a web component, it will mixin from component
+	var extend = {
+		//we'll be extended component.create
+		//the inherited function is passed in as the first param
+		create: function (sup) {
 
-    //define a web component, it will mixin from component
-    var extend = {
-        //we'll be extended component.create
-        //the inherited function is passed in as the first param
-        create: function(sup) {
+			if (!this.tag) {
+				throw "need .tag property when using mixin webcomponent"
+			};
+			var _this = this;
 
-            if(!this.tag) { throw "need .tag property when using mixin webcomponent" };
-            var _this = this;
+			this.instance = document.registerElement(
+				this.tag, {
+					prototype: Object.create(
+						_this, {
+							createdCallback: {
+								value: function () {
+									//run inherited create
+									sup.apply(this, arguments);
+									//fill the node with it's template
+									this.innerHTML += _this.generateInnerHTML();
 
-            this.instance = document.registerElement(
-              this.tag,
-              {
-                prototype: Object.create(
-                  _this, {
-                  createdCallback: {value: function() {
-                    //run inherited create
-                    sup.apply(this, arguments);
-                    //fill the node with it's template
-                    this.innerHTML += _this.generateInnerHTML();
-                    messages.pub(this.channel + '/rendered');
-                  }},
+									if (this.created && typeof this.created == 'function') {
+										this.created();
+									}
+									//putting this on the backburner for now
+									//messages.pub(this.channel + '/rendered');
+								}
+							},
 
-                  //other call backs that need to be attached somewhere
-                  //todo add to messaging
-                  attachedCallback: {value: function() {
-                    console.log('live on DOM ;-) ');
-                  }},
-                  detachedCallback: {value: function() {
-                    console.log('leaving the DOM :-( )');
-                  }},
-                  attributeChangedCallback: {value: function(
-                    name, previousValue, value
-                  ) {
-                    if (previousValue == null) {
-                      console.log(
-                        'got a new attribute ', name,
-                        ' with value ', value
-                      );
-                    } else if (value == null) {
-                      console.log(
-                        'somebody removed ', name,
-                        ' its value was ', previousValue
-                      );
-                    } else {
-                      console.log(
-                        name,
-                        ' changed from ', previousValue,
-                        ' to ', value
-                      );
-                    }
-                  }}
-                })
-              }
-            );
-        }
-    };
+							//other call backs that need to be attached somewhere
+							//todo add to messaging
+							attachedCallback: {
+								value: function () {
+									if (this.attached && typeof this.attached == 'function') {
+										this.attached();
+									}
+								}
+							},
+							detachedCallback: {
+								value: function () {
+									if (this.detached && typeof this.detached == 'function') {
+										this.detached();
+									}
+								}
+							},
+							attributeChangedCallback: {
+								value: function (
+									name, previousValue, value
+								) {
+									if (previousValue == null) {
+										console.log(
+											'got a new attribute ', name,
+											' with value ', value
+										);
+									} else if (value == null) {
+										console.log(
+											'somebody removed ', name,
+											' its value was ', previousValue
+										);
+									} else {
+										console.log(
+											name,
+											' changed from ', previousValue,
+											' to ', value
+										);
+									}
+								}
+							}
+						})
+				}
+			);
+		}
+	};
 
-    webcomponent = mixins.mix(webcomponent, extend);
-    return webcomponent;
+	webcomponent = mixins.mix(webcomponent, extend);
+	return webcomponent;
 });
